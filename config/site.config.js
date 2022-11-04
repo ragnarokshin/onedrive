d127a369cc162ee1c64c90ba77e4f178f9ca60e1
fanzyb <kojedb@secret.fyi>
2022-11-05 06:47:05 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -59,6 +59,10 @@ module.exports = {
       name: 'Telegram',
       link: 'https://t.me/madshinichiro',
     },
+    {
+      name: 'Telegram',
+      link: 'https://t.me/ragnarokdrive',
+    },
   ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
